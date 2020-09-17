15ae08cac3da9e0480738c648c989033ce718125
teamgecko17hacker01 <71354896+teamgecko17hacker01@users.noreply.github.com>
2020-09-17 09:50:01 -0400

    Update Program.cs

--- apis/poi/web/Program.cs
+++ apis/poi/web/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using poi.Utility;
+fail
 namespace poi
 {
     public class Program
