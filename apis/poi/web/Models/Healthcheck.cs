72b71f73a9862387b303a912db587bbc37c81261
team-gecko-17-hacker5 <71354419+team-gecko-17-hacker5@users.noreply.github.com>
2020-09-17 17:26:57 -0400

    Update Healthcheck.cs

--- apis/poi/web/Models/Healthcheck.cs
+++ apis/poi/web/Models/Healthcheck.cs
@@ -7,7 +7,7 @@ namespace poi.Models
     {
         public Healthcheck()
         {
-            Message = "POI Service Healthcheck";
+            Message = "POI Service Healthcheck - Blue party.";
             Status = "Healthy";
         }
         [Newtonsoft.Json.JsonProperty(PropertyName = "message")]
